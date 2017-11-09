56698e3e2d04f4dac9d187b9024273aeef40e964
Stefan Müller <mullers@tcd.ie>
2017-11-09 14:38:51 +0000

    Use packman package to install and load packages

--- app.R
+++ app.R
@@ -1,18 +1,21 @@
 
-# Load the app here
+# Use packman package to check whether required packages are installed.
+# Install missing packages, and load all packages afterwards
 
-library(shiny)
-library(shinydashboard)
-library(quanteda)
-library(ggplot2)
-library(tidyverse)
-library(readxl)
-library(haven)
-library(stringr)
-library(topicmodels)
-library(data.table)
-library(ggridges)
-library(shinyjs)
+if (!require("pacman")) install.packages("pacman")
+
+pacman::p_load(shiny, 
+               shinydashboard,
+               shinyjs,
+               quanteda,
+               ggplot2,
+               dplyr,
+               data.table,
+               stringr,
+               readxl,
+               haven,
+               topicmodels,
+               ggridges)
 
 source("ui_tada.R", local = TRUE)$value
 source("server_tada.R", local = TRUE)$value
