4826818cec056e414756913f08c3954ecbd26697
Michael Jankowski <michael.jankowski@uni-oldenburg.de>
2017-12-05 13:53:26 +0100

    fixed some things for shinyapps...

--- app.R
+++ app.R
@@ -1,33 +1,32 @@
 # Use packman package to check whether required packages are installed.
 # Install missing packages, and load all packages afterwards
 
-library(shiny)
-library(shinydashboard)
-library(shinyjs)
-library(quanteda)
-library(ggplot2)
-library(data.table)
-library(stringr)
-library(readxl)
-library(haven)
-library(topicmodels)
-library(ggridges)
+if(!require(pacman)) install.packages("pacman")
 
-#if (!require("pacman")) install.packages("pacman")
+#library(shiny)
+#library(shinydashboard)
+#library(shinyjs)
+#library(quanteda)
+#library(ggplot2)
+#library(data.table)
+#library(stringr)
+#library(readxl)
+#library(haven)
+#library(topicmodels)
+#library(ggridges)
 
-#pacman::p_load(shiny, 
-#               shinydashboard,
-#               shinyjs,
-#               quanteda,
-#               ggplot2,
-#               dplyr,
-#               data.table,
-#               stringr,
-#               readxl,
-#               haven,
-#               topicmodels,
-#               ggridges,
-#               install = FALSE)
+pacman::p_load(shiny, 
+               shinydashboard,
+               shinyjs,
+               quanteda,
+               ggplot2,
+               dplyr,
+               data.table,
+               stringr,
+               readxl,
+               haven,
+               topicmodels,
+               ggridges)
 
 source("ui_tada.R", local = TRUE)$value
 source("server_tada.R", local = TRUE)$value
